8d77db29fc95fae0e17c64c4248d1ee07b92d6a1
David Christenson <mail@noctem.xyz>
2019-02-24 20:51:35 -0700

    Use Expr for chroma adjustments
    The custom x86 assembly in std.Expr is faster than I can get LLVM or
    GCC to make this.

--- adjust.c
+++ adjust.c
@@ -1,17 +1,18 @@
 #include <math.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#include <vapoursynth/VapourSynth.h>
 #include <vapoursynth/VSHelper.h>
+#include <vapoursynth/VapourSynth.h>
 
 static double radians(double degrees) { return degrees * M_PI / 180.0; }
 
 typedef struct adjustData {
     VSNodeRef *node;
     const VSVideoInfo *vi;
-    int adjustLuma, chromaHeight, chromaStride, chromaWidth, height, stride, width;
-    double mult1, mult2;
+    int adjustLuma, chromaHeight, chromaStride, chromaWidth, height, stride,
+        width;
     uint8_t lut[256];
 } adjustData;
 
@@ -31,47 +32,28 @@ static const VSFrameRef *VS_CC adjustGetFrame(
         vsapi->requestFrameFilter(n, d->node, frameCtx);
     } else if (activationReason == arAllFramesReady) {
         const VSFrameRef *src = vsapi->getFrameFilter(n, d->node, frameCtx);
-        VSFrameRef *dst = vsapi->newVideoFrame(d->vi->format, d->width, d->height, src, core);
+        VSFrameRef *dst =
+            vsapi->newVideoFrame(d->vi->format, d->width, d->height, src, core);
 
         const uint8_t *ysrc = vsapi->getReadPtr(src, 0);
         uint8_t *ydst = vsapi->getWritePtr(dst, 0);
 
         if (d->adjustLuma) {
             for (int y = 0; y < d->height; y++) {
-                for (int x = 0; x < d->width; x++)
-                    ydst[x] = d->lut[ysrc[x]];
+                for (int x = 0; x < d->width; x++) ydst[x] = d->lut[ysrc[x]];
                 ysrc += d->stride;
                 ydst += d->stride;
             }
         } else
             vs_bitblt(ydst, d->stride, ysrc, d->stride, d->width, d->height);
 
-        const uint8_t *usrc = vsapi->getReadPtr(src, 1);
-        const uint8_t *vsrc = vsapi->getReadPtr(src, 2);
-        uint8_t *udst = vsapi->getWritePtr(dst, 1);
-        uint8_t *vdst = vsapi->getWritePtr(dst, 2);
+        vs_bitblt(vsapi->getWritePtr(dst, 1), d->chromaStride,
+                  vsapi->getReadPtr(src, 1), d->chromaStride, d->chromaWidth,
+                  d->chromaHeight);
+        vs_bitblt(vsapi->getWritePtr(dst, 2), d->chromaStride,
+                  vsapi->getReadPtr(src, 2), d->chromaStride, d->chromaWidth,
+                  d->chromaHeight);
 
-        if (d->mult1 != 1.0 || d->mult2 != 0.0) {
-            double u, u2, v, v2;
-
-            for (int y = 0; y < d->chromaHeight; y++) {
-                for (int x = 0; x < d->chromaWidth; x++) {
-                    u = usrc[x] - 128.0;
-                    v = vsrc[x] - 128.0;
-                    u2 = u * d->mult1 + v * d->mult2 + 128.5;
-                    v2 = v * d->mult1 - u * d->mult2 + 128.5;
-                    udst[x] = (u2 < 255.0) ? ((u2 >= 1.0) ? (uint8_t)u2 : 0) : 255;
-                    vdst[x] = (v2 < 255.0) ? ((v2 >= 1.0) ? (uint8_t)v2 : 0) : 255;
-                }
-                usrc += d->stride;
-                vsrc += d->stride;
-                udst += d->stride;
-                vdst += d->stride;
-            }
-        } else {
-            vs_bitblt(udst, d->chromaStride, usrc, d->chromaStride, d->chromaWidth, d->chromaHeight);
-            vs_bitblt(vdst, d->chromaStride, vsrc, d->chromaStride, d->chromaWidth, d->chromaHeight);
-        }
         vsapi->freeFrame(src);
 
         return dst;
@@ -81,9 +63,107 @@ static const VSFrameRef *VS_CC adjustGetFrame(
 }
 
 static void VS_CC adjustInit(VSMap *in, VSMap *out, void **instanceData,
-                            VSNode *node, VSCore *core, const VSAPI *vsapi) {
+                             VSNode *node, VSCore *core, const VSAPI *vsapi) {
     adjustData *d = (adjustData *)*instanceData;
     vsapi->setVideoInfo(d->vi, 1, node);
+}
+
+static VSNodeRef *tweakChroma(VSNodeRef *node, double mult1, double mult2,
+                              VSColorFamily colorFamily, VSPlugin *std,
+                              VSMap *out, const VSAPI *vsapi) {
+    char buf[128];
+
+    if (std == NULL) {
+        vsapi->setError(out, "adjust: standard plugin not found");
+        return NULL;
+    }
+    VSMap *args = vsapi->createMap();
+    vsapi->propSetNode(args, "clips", node, paReplace);
+    vsapi->propSetInt(args, "planes", 1, paReplace);
+    vsapi->propSetInt(args, "colorfamily", cmGray, paReplace);
+
+    VSMap *uPlaneMap = vsapi->invoke(std, "ShufflePlanes", args);
+    if (vsapi->getError(uPlaneMap)) {
+        vsapi->setError(out, vsapi->getError(uPlaneMap));
+        vsapi->freeMap(uPlaneMap);
+        vsapi->freeMap(args);
+        return NULL;
+    }
+    VSNodeRef *usrc = vsapi->propGetNode(uPlaneMap, "clip", 0, NULL);
+    vsapi->freeMap(uPlaneMap);
+
+    vsapi->propSetInt(args, "planes", 2, paReplace);
+    VSMap *vPlaneMap = vsapi->invoke(std, "ShufflePlanes", args);
+    if (vsapi->getError(vPlaneMap)) {
+        vsapi->setError(out, vsapi->getError(vPlaneMap));
+        vsapi->freeMap(vPlaneMap);
+        vsapi->freeMap(args);
+        return NULL;
+    }
+    VSNodeRef *vsrc = vsapi->propGetNode(vPlaneMap, "clip", 0, NULL);
+    vsapi->freeMap(vPlaneMap);
+
+    vsapi->clearMap(args);
+    vsapi->propSetNode(args, "clips", usrc, paReplace);
+    vsapi->propSetNode(args, "clips", vsrc, paAppend);
+
+    snprintf(buf, 128, "x 128 - %f * y 128 - %f * + 128 + 0 max 255 min", mult1,
+             mult2);
+    vsapi->propSetData(args, "expr", buf, -1, paReplace);
+
+    uPlaneMap = vsapi->invoke(std, "Expr", args);
+    if (vsapi->getError(uPlaneMap)) {
+        vsapi->setError(out, vsapi->getError(uPlaneMap));
+        vsapi->freeMap(uPlaneMap);
+        vsapi->freeMap(args);
+        vsapi->freeNode(node);
+        vsapi->freeNode(usrc);
+        vsapi->freeNode(vsrc);
+        return NULL;
+    }
+    VSNodeRef *udst = vsapi->propGetNode(uPlaneMap, "clip", 0, NULL);
+    vsapi->freeMap(uPlaneMap);
+
+    snprintf(buf, 128, "y 128 - %f * x 128 - %f * - 128 + 0 max 255 min", mult1,
+             mult2);
+    vsapi->propSetData(args, "expr", buf, -1, paReplace);
+
+    vPlaneMap = vsapi->invoke(std, "Expr", args);
+    vsapi->freeNode(usrc);
+    vsapi->freeNode(vsrc);
+    if (vsapi->getError(vPlaneMap)) {
+        vsapi->setError(out, vsapi->getError(vPlaneMap));
+        vsapi->freeMap(vPlaneMap);
+        vsapi->freeMap(args);
+        vsapi->freeNode(node);
+        return NULL;
+    }
+    VSNodeRef *vdst = vsapi->propGetNode(vPlaneMap, "clip", 0, NULL);
+    vsapi->freeMap(vPlaneMap);
+
+    vsapi->clearMap(args);
+    vsapi->propSetNode(args, "clips", node, paReplace);
+    vsapi->propSetNode(args, "clips", udst, paAppend);
+    vsapi->propSetNode(args, "clips", vdst, paAppend);
+    vsapi->propSetInt(args, "planes", 0, paReplace);
+    vsapi->propSetInt(args, "planes", 0, paAppend);
+    vsapi->propSetInt(args, "planes", 0, paAppend);
+    vsapi->propSetInt(args, "colorfamily", colorFamily, paReplace);
+
+    VSMap *tweakedMap = vsapi->invoke(std, "ShufflePlanes", args);
+    vsapi->freeMap(args);
+    vsapi->freeNode(node);
+    vsapi->freeNode(udst);
+    vsapi->freeNode(vdst);
+    if (vsapi->getError(tweakedMap)) {
+        vsapi->setError(out, vsapi->getError(tweakedMap));
+        vsapi->freeMap(tweakedMap);
+        return NULL;
+    }
+    VSNodeRef *tweaked = vsapi->propGetNode(tweakedMap, "clip", 0, NULL);
+    vsapi->freeMap(tweakedMap);
+
+    return tweaked;
 }
 
 static void VS_CC adjustCreate(const VSMap *in, VSMap *out, void *userData,
@@ -92,24 +172,18 @@ static void VS_CC adjustCreate(const VSMap *in, VSMap *out, void *userData,
     adjustData *data;
     int err;
 
-    d.node = vsapi->propGetNode(in, "clip", 0, NULL);
-    d.vi = vsapi->getVideoInfo(d.node);
+    VSNodeRef *node = vsapi->propGetNode(in, "clip", 0, NULL);
+    d.vi = vsapi->getVideoInfo(node);
 
-    const VSFrameRef *frame = vsapi->getFrame(0, d.node, NULL, 0);
+    const VSFrameRef *frame = vsapi->getFrame(0, node, NULL, 0);
     d.stride = vsapi->getStride(frame, 0);
     d.chromaStride = vsapi->getStride(frame, 1);
     vsapi->freeFrame(frame);
 
     d.height = d.vi->height;
     d.width = d.vi->width;
-
-    if (d.vi->format->subSamplingH || d.vi->format->subSamplingW) {
-        d.chromaHeight = d.height >> d.vi->format->subSamplingH;
-        d.chromaWidth = d.width >> d.vi->format->subSamplingW;
-    } else {
-        d.chromaHeight = d.height;
-        d.chromaWidth = d.width;
-    }
+    d.chromaHeight = d.height >> d.vi->format->subSamplingH;
+    d.chromaWidth = d.width >> d.vi->format->subSamplingW;
 
     double hue = vsapi->propGetFloat(in, "hue", 0, &err);
     if (err) hue = 0.0;
@@ -125,17 +199,22 @@ static void VS_CC adjustCreate(const VSMap *in, VSMap *out, void *userData,
     } else {
         d.adjustLuma = 1;
         numLuma = vsapi->propNumElements(in, "luma");
-        if (numLuma != 1)
-            incr = 256 / (numLuma - 1);
+        if (numLuma != 1) incr = 256 / (numLuma - 1);
     }
 
-    hue = radians(hue);
-    d.mult1 = cos(hue) * sat;
-    d.mult2 = sin(hue) * sat;
+    if (hue != 0.0 || sat != 1.0) {
+        hue = radians(hue);
+        d.node = tweakChroma(
+            node, cos(hue) * sat, sin(hue) * sat, d.vi->format->colorFamily,
+            vsapi->getPluginById("com.vapoursynth.std", core), out, vsapi);
+
+        if (d.node == NULL) return;
+    } else
+        d.node = node;
 
     double min_mult, max_mult, a, b, val;
     int start, end;
-    
+
     if (numLuma == 1) {
         double mult = 1.0 + (lumaAdjust[0] / 100.0);
         for (int i = 0; i < 256; i++) {
@@ -150,13 +229,14 @@ static void VS_CC adjustCreate(const VSMap *in, VSMap *out, void *userData,
             else
                 end = (i + 1) * incr - 1;
             min_mult = 1.0 + (lumaAdjust[i] / 100.0);
-            max_mult = 1.0 + (lumaAdjust[i+1] / 100.0);
+            max_mult = 1.0 + (lumaAdjust[i + 1] / 100.0);
             a = (max_mult - min_mult) / (end - start);
             b = max_mult - a * end;
 
             for (int j = start; j <= end; j++) {
                 val = ((a * j + b) * j) + 0.5;
-                d.lut[j] = (val < 255.0) ? ((val >= 1.0) ? (uint8_t)val : 0) : 255;
+                d.lut[j] =
+                    (val < 255.0) ? ((val >= 1.0) ? (uint8_t)val : 0) : 255;
             }
         }
     }
@@ -173,5 +253,7 @@ VapourSynthPluginInit(VSConfigPlugin configFunc,
                       VSRegisterFunction registerFunc, VSPlugin *plugin) {
     configFunc("xyz.noctem.adjust", "adjust", "Filter for adjusting",
                VAPOURSYNTH_API_VERSION, 1, plugin);
-    registerFunc("adjust", "clip:clip;sat:float:opt;hue:float:opt;luma:float[]:opt", adjustCreate, NULL, plugin);
+    registerFunc("adjust",
+                 "clip:clip;sat:float:opt;hue:float:opt;luma:float[]:opt",
+                 adjustCreate, NULL, plugin);
 }
